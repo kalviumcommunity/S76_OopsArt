144799399bcdbe01a35fcbd957ccc61fc86b37fc
sandanam <santhanamk9604@gmail.com>
2025-02-12 10:37:00 +0530

    added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const dotenv =require("dotenv");
-const connectDatabase = require('./database');
+const {connectDatabase,getConnection} = require('./database');
 
 app.get('/ping', (req, res) => {
     res.send('Pong!');
@@ -11,6 +11,9 @@ app.get('/ping', (req, res) => {
 
 connectDatabase();
 
+app.get('/',(req,res)=>{
+    res.send(getConnection())
+})
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
