9bef2c6873f7308e2026f41fa22879feabad1d4d
sandanam <santhanamk9604@gmail.com>
2025-03-22 12:15:00 +0530

    added mongoatlas

--- OneDrive/Desktop/asap/S76_OopsArt/database.js
+++ OneDrive/Desktop/asap/S76_OopsArt/database.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-
 const connectDatabase = async () => {
     try {
         const connection = await mongoose.connect(process.env.DB_URL, );
