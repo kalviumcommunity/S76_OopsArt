68f3a91f3c49c251d06fba3c9b96c0cb42ce6553
Sandanam .k <santhanamk9604@gmail.com>
2025-03-27 17:29:56 +0530

    Create CourseCard.jsx

---
+++ client/src/components/CourseCard.jsx
@@ -0,0 +1,13 @@
+function CourseCard ({ course }) {
+    
+    return (
+      <div className="border p-4 rounded-lg shadow-md">
+        <h2 className="text-xl font-semibold">{course.title}</h2>
+        <p className="text-gray-600">{course.description}</p>
+        <p className="text-sm text-blue-500">Category: {course.category}</p>
+      </div>
+    );
+  };
+
+  export default CourseCard;
+  
